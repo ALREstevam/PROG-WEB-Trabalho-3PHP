e4f7eda56f45e809a5a5e4fcb2dc619c089f81dd
Huanna <huanna_nascimento@outlook.com>
2017-11-23 11:01:02 -0200

    commit

--- site/php_control/DbSetup.class.php
+++ site/php_control/DbSetup.class.php
@@ -9,12 +9,24 @@ require_once 'DbConnection.class.php';
 
 
 class DbSetup{
-	
 
-$DB = new DbConnection();
-$conn = $DB -> connect("information_schema", DB_HOST, DB_PASSWORD, DB_USERNAME);
+function setup(){	
+
+$db = new DbConnection();
+$conn = $db -> connect("information_schema", DB_HOST, DB_PASSWORD, DB_USERNAME);
+$sql = file_get_contents ('../sql/db_pw3.sql');
+var_dump($sql);
+
+$teste = $conn -> exec('$sql');
+var_dump($teste);
+
 }
 
+}
+ 
+$obj = new DbSetup();
+$aux = $obj -> setup();
+var_dump($aux); 
 
 
 ?>
