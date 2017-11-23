8ca22dff6f7efe2d7e2663bff695cf65c4d51c18
Huanna <huanna_nascimento@outlook.com>
2017-11-23 11:04:07 -0200

    Huanna

--- site/php_control/DbSetup.class.php
+++ site/php_control/DbSetup.class.php
@@ -15,19 +15,11 @@ function setup(){
 $db = new DbConnection();
 $conn = $db -> connect("information_schema", DB_HOST, DB_PASSWORD, DB_USERNAME);
 $sql = file_get_contents ('../sql/db_pw3.sql');
-var_dump($sql);
-
-$teste = $conn -> exec('$sql');
-var_dump($teste);
 
 }
 
 }
  
-$obj = new DbSetup();
-$aux = $obj -> setup();
-var_dump($aux); 
-
 
 ?>
 
