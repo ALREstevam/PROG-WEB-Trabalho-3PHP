027c35ab12a8674362a38fb329ba1fe080f61e32
Huanna <huanna_nascimento@outlook.com>
2017-11-27 19:05:18 -0200

    i

--- site/php_control/UserControl.class.php
+++ site/php_control/UserControl.class.php
@@ -15,14 +15,22 @@ require_once "DbConnection.class.php";
 
 
 class UserControl{
-    function addUpdateUser(User $usr){
 
-        return true;
+    /**
+     * Adicionar um usuário se o CPF não existir, atualizar se já exisitr (testar se é novo ou não e chamar
+     * addUser OU updateUser)
+     * @param User $usr
+     */
+    function addUpdateUser(User $usr){
+		
+		
+		
+        
     }
 
     function addUser(User $usr){
 
-        return true;
+
     }
 
     function updateUser(User $usr){
@@ -32,41 +40,19 @@ class UserControl{
     function retrieveAllUsers(){   //KAREN
         $BD = new DbConnection();
 		$conection = $BD->connectWithConsts();
-		$result = $conection->query("SELECT * FROM tbl_users");
-		$result->fetchAll();
+		$conection->exec( SELECT * FROM tbl_users );
     }
 
-    function retrieveUserById($id){
-        $dbc = new DbConnection();
-        $conn = $dbc->connectWithConsts();
-        if($conn != null){
-            try{
-                $stmt = $conn->query("SELECT * FROM tbl_users WHERE PK_cpf = '$id'");
-                while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                    return $this->toUserObject($row);
-                }
-            }catch (Exception $e){
-                echo "Erro ao recuperar usuário: $e";
-                return null;
-            }
-        }
-        return null;
-    }
-
-    function retrieveLoggedUser(){
-        /*
-         * Retornar um objeto User se houver algum logado ou false caso contrário
-         * */
-
-        return new User("Ana", 'abc', 'maisid@il66', '1', '2013-01-08', '123', '3568-989', 'Rua 2', '566', 'Europa', '', '1256-000', 'Bairro a', 'Portugal', true);
+    function retrieveUser($id){
+        return new User();
     }
 
     function toUserObject($OriginalData){    //KAREN
-         return null;
+         
     }
 
     function authUser($login, $password){
-        return false;
+        return -1;
     }
 
     function loginUser($uid){
@@ -78,6 +64,6 @@ class UserControl{
     }
 
     function isUserLogged(){
-        return true;
+
     }
 }
